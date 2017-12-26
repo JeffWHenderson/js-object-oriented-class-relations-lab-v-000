5f0bd7136062bdc23a60547ca53552b4b10ce077
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-26 22:00:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ class Driver {
     store.drivers.push(this)
   }//end of constructor
 
-  trips(){
-    return store.trips.filter(trip => {
-      return trip.driverId == this.id;
-    });
+  trips() {
+      return store.trips.filter(function(trip){
+        return trip.driverId === this.id
+      })
   }
+
+  // trips(){
+  //   return store.trips.filter(trip => {
+  //     return trip.driverId == this.id;
+  //   });
+  // }
   passengers(){
     return this.trips().map(trip => {
       console.log(trip.passenger())

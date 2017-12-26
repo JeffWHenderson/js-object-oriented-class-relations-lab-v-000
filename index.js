dd7a69b7595da5c764a2ae7a76167bc311a9ed0d
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-26 20:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let store = { drivers: [], passengers: [] }
+let store = { drivers: [], passengers: [], trips: [] }
 let driverId = 0
 let passengerId = 0
 let tripId = 0
@@ -22,5 +22,9 @@ class Passenger {
 class Trip {
   constructor(driver, passenger) {
     this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+
+    store.trips.push(this)
   }
 } // end of Trip Object
